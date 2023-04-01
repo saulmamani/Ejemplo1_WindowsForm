18ccf7edb290da73935c44594808781ca8773931
saulmamani <luas0_1@yahoo.es>
2023-04-01 08:09:36 -0400

    operacion de numeros con windows form

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace UI_ejemplo_1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPrimos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSumar.Location = new System.Drawing.Point(34, 342);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(320, 70);
+            this.btnSumar.TabIndex = 0;
+            this.btnSumar.Text = "Calcular";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OPERACIONES";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(34, 144);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(320, 39);
+            this.txtN1.TabIndex = 2;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(34, 254);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(320, 39);
+            this.txtN2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numero 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Numero 2:";
+            // 
+            // btnPrimos
+            // 
+            this.btnPrimos.Location = new System.Drawing.Point(34, 481);
+            this.btnPrimos.Name = "btnPrimos";
+            this.btnPrimos.Size = new System.Drawing.Size(320, 84);
+            this.btnPrimos.TabIndex = 6;
+            this.btnPrimos.Text = "Mostrar numeros primos";
+            this.btnPrimos.UseVisualStyleBackColor = true;
+            this.btnPrimos.Click += new System.EventHandler(this.btnPrimos_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(397, 662);
+            this.Controls.Add(this.btnPrimos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSumar);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPrimos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_ejemplo_1
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txtN1.Text);
+            int numero2 = Convert.ToInt32(txtN2.Text);
+
+            int suma = Sumar(numero1, numero2);
+            int resta = Restar(numero1, numero2);
+            int multiplicacion = Multiplicar(numero1, numero2);
+            double division = Dividir(numero1, numero2);
+
+            MessageBox.Show($"La suma es: {suma}\n" +
+                $"La resta es: {resta}\n" +
+                $"La multiplicacion es: {multiplicacion}\n" +
+                $"La division es: {division}");
+        }
+
+        private double Dividir(int numero1, int numero2)
+        {
+            return numero1 / (double)numero2;
+        }
+
+        private int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        private int Restar(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        private int Sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+        
+        private void btnPrimos_Click(object sender, EventArgs e)
+        {
+           //TODO Dado un rango de valores (numero1 - numero2), mostrar en 
+           //es un mensaje (MessageBox.Show) los numeros primos dentro el trando
+           
+        }
+    }
+}
